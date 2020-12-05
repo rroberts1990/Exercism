0e4fc71ad7b9a034f022e7fba6024664a8b75283
Ross Roberts <ross123roberts@gmail.com>
2020-12-05 15:27:53 -0500

    new exercises

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,8 +1,5 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError('strands must be of equal length')
-    hamming = 0
-    for i,v in enumerate(strand_a):
-        hamming += 1 if v != strand_b[i] else 0
-    return hamming
+    return sum([ch1 != ch2 for ch1, ch2 in zip(strand_a, strand_b)])
 
